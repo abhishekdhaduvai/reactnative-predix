5b162d917ca32f33e6c5b16036dde2721deccf06
Abhishek Dhaduvai <abhishek.dhaduvai1@gmail.com>
2018-03-26 11:00:28 -0700

    update config

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-// export const credentials = {
-//   uaaURL: 'your-UAA-url',
-//   clientID: 'your-client-name',
-//   base64ClientCredentials: 'your-base64-credentials'
-// }
 export const credentials = {
   uaaURL: 'https://c2b4d7c7-af50-4c49-a03f-00a004102c69.predix-uaa.run.aws-usw02-pr.ice.predix.io',
   clientID: 'reactNative',
